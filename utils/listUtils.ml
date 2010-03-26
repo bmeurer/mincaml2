4741be64744c98657ee8b893cf75d702dbfe47eb
Benedikt Meurer <benedikt.meurer@googlemail.com>
2010-03-26 21:10:17 +0100

    Add ListUtils.make.

--- utils/listUtils.ml
+++ utils/listUtils.ml
@@ -1,3 +1,13 @@
+let make n x =
+  let rec make_aux accu = function
+    | 0 -> accu
+    | n -> make_aux (x :: accu) (n - 1)
+  in
+    if n < 0 then
+      invalid_arg "ListUtils.make"
+    else
+      make_aux [] n
+  
 let swap n l =
   let rec swap_aux n l accu =
     match n, l with
--- utils/listUtils.mli
+++ utils/listUtils.mli
@@ -1,3 +1,7 @@
+(** [ListUtils.make n x] creates a new list which contains the value [x] [n] times.
+    Raise [Invalid_argument "ListUtils.make"] if [n] is negative. *)
+val make: int -> 'a -> 'a list
+
 (** [ListUtils.swap n l] swaps the [0]-th and the [n]-th element of the list [l].
     Raise [Failure "ListUtils.swap"] if the list [l] is too short.
     Raise [Invalid_argument "ListUtils.swap"] if [n] is negative. *)
