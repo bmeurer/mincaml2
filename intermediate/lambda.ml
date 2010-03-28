551e65febe39c82ba47e39ed250c4c153b72a5d1
Benedikt Meurer <benedikt.meurer@googlemail.com>
2010-03-28 13:10:18 +0200

    Implement Lambda.subst.

--- intermediate/lambda.ml
+++ intermediate/lambda.ml
@@ -52,5 +52,41 @@ and lambda_switch =
 
 let lambda_unit = Lconst(Sconst_pointer 0)
 
-let rec subst id lambda lambda' =
-  assert false (* TODO *)
+let rec subst id' lambda' = function
+  | Lconst(_) as lambda ->
+      lambda
+  | Lident(id) when Ident.equal id id' ->
+      lambda'
+  | Lident(_) as lambda ->
+      lambda
+  | Llet(id, lambda1, lambda2) when Ident.equal id id' ->
+      Llet(id, subst id' lambda' lambda1, lambda2)
+  | Llet(id, lambda1, lambda2) ->
+      Llet(id, subst id' lambda' lambda1, subst id' lambda' lambda2)
+  | Lprim(prim, lambdal) ->
+      Lprim(prim, List.map (subst id' lambda') lambdal)
+  | Lcond(lambda, cond) ->
+      Lcond(subst id' lambda' lambda,
+            { cond with
+                cond_cases = List.map (fun (c, lambda) -> c, subst id' lambda' lambda) cond.cond_cases;
+                cond_default = subst id' lambda' cond.cond_default })
+  | Lswitch(lambda, switch) ->
+      Lswitch(subst id' lambda' lambda,
+              { switch with
+                  sw_consts = List.map (fun (i, lambda) -> i, subst id' lambda' lambda) switch.sw_consts;
+                  sw_blocks = List.map (fun (i, lambda) -> i, subst id' lambda' lambda) switch.sw_blocks;
+                  sw_default = (match switch.sw_default with
+                                  | None -> None
+                                  | Some(lambda) -> Some(subst id' lambda' lambda)) })
+  | Lstaticraise as lambda ->
+      lambda
+  | Lstaticcatch(lambda1, lambda2) ->
+      Lstaticcatch(subst id' lambda' lambda1,
+                   subst id' lambda' lambda2)
+  | Lifthenelse(lambda0, lambda1, lambda2) ->
+      Lifthenelse(subst id' lambda' lambda0,
+                  subst id' lambda' lambda1,
+                  subst id' lambda' lambda2)
+  | Lsequence(lambda1, lambda2) ->
+      Lsequence(subst id' lambda' lambda1,
+                subst id' lambda' lambda2)
