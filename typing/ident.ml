376c389ff8e297d9ce88a56ad5f2e40066eb9413
Benedikt Meurer <benedikt.meurer@googlemail.com>
2010-04-03 21:32:32 +0200

    Generate safe unique_name's.

--- typing/ident.ml
+++ typing/ident.ml
@@ -21,10 +21,20 @@ let name id =
   id.name
 
 let unique_name id =
-  if id.stamp >= 0 then
-    id.name ^ "__" ^ (string_of_int id.stamp)
-  else
-    "mc2_" ^ id.name
+  let s = (if id.stamp >= 0 then
+             id.name ^ "__" ^ (string_of_int id.stamp)
+           else
+             "mc2_" ^ id.name) in
+  let b = Buffer.create (String.length s) in
+    for i = 0 to String.length s - 1 do
+      match s.[i] with
+        | 'A'..'Z' | 'a'..'z' | '0'..'9' | '_' as c ->
+            Buffer.add_char b c
+        | _ as c ->
+            Buffer.add_char b '$';
+            Buffer.add_string b (Printf.sprintf "%02x" (Char.code c))
+    done;
+    Buffer.contents b
 
 let compare id1 id2 =
   let c = id1.stamp - id2.stamp in
